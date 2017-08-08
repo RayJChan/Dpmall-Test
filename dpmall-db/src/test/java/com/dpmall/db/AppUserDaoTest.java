a87b2d1672a8c5457453c695ac6daf5265f89d2a
weijie_chan1993@163.com <weijie_chan1993@163.com>
2017-08-08 09:16:10 +0800

    modified1

--- dpmall-db/src/test/java/com/dpmall/db/AppUserDaoTest.java
+++ dpmall-db/src/test/java/com/dpmall/db/AppUserDaoTest.java
@@ -47,6 +47,10 @@ public class AppUserDaoTest extends SpringTestCase {
 		
 		LOG.info(JSON.toJSONString(appUserDao.getStoreAllUser(2L)));
 	}
+	@Test
+	public void getLL(){
+		LOG.info(JSON.toJSONString(appUserDao.getStoreAllUser(2L)));
+	}
 	
 	
 }
